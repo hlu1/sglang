5aac26e42fa3554a8776ac53f182fcca7947b201
Hao Lu <14827759+hlu1@users.noreply.github.com>
2025-10-31 18:42:44 -0700

    wip

--- benchmark/gsm8k/bench_sglang.py
+++ benchmark/gsm8k/bench_sglang.py
@@ -59,13 +59,26 @@ def main(args):
     num_shots = args.num_shots
     few_shot_examples = get_few_shot_examples(lines, num_shots)
 
+    # questions = []
+    # labels = []
+    # for i in range(len(lines[:num_questions])):
+    #     questions.append(get_one_example(lines, i, False))
+    #     labels.append(get_answer_value(lines[i]["answer"]))
+    # assert all(l != INVALID for l in labels)
+    # arguments = [{"question": q} for q in questions]
+
+
+    bad_questions = [34, 37, 46, 58, 62, 93, 97, 102, 107, 108, 119, 149, 155, 157, 172, 175, 189, 193, 198, 199, 204, 205, 209, 210, 219, 236, 238, 241, 242, 243, 255, 257, 267, 281, 290, 293, 298, 308, 313, 331, 342, 353, 357, 368, 371, 382, 392, 394, 403, 406, 409, 423, 425, 427, 428, 432, 439, 454, 457, 459, 466, 468, 470, 502, 528, 539, 564, 568, 570, 574, 580, 590, 635, 640, 641, 649, 651, 652, 675, 696, 727, 738, 739, 749, 754, 755, 768, 780, 782, 785, 796, 802, 809, 814, 815, 823, 827, 831, 835, 857, 858, 877, 883, 886, 894, 897, 900, 912, 923, 925, 926, 931, 938, 943, 951, 952, 953, 956, 960, 962, 972, 976, 979, 984, 991, 997, 1014, 1016, 1019, 1021, 1035, 1038, 1039, 1042, 1046, 1048, 1059, 1068, 1070, 1074, 1079, 1088, 1110, 1118, 1120, 1137, 1145, 1157, 1159, 1161, 1166, 1174, 1181, 1182, 1183, 1190, 1191, 1199, 1212, 1216, 1231, 1249, 1251, 1273, 1281, 1288, 1298, 1302, 1306, 1309, 1310]
+
     questions = []
     labels = []
-    for i in range(len(lines[:num_questions])):
+    for i in range(len(bad_questions)):
         questions.append(get_one_example(lines, i, False))
         labels.append(get_answer_value(lines[i]["answer"]))
     assert all(l != INVALID for l in labels)
     arguments = [{"question": q} for q in questions]
+
+
 
     #####################################
     ######### SGL Program Begin #########
@@ -97,6 +110,30 @@ def main(args):
     preds = []
     for i in range(len(states)):
         preds.append(get_answer_value(states[i]["answer"]))
+
+    #     if preds[i] == labels[i]:
+    #         print(f"Question: {questions[i]}")
+    #         print(f"Pred: {preds[i]}")
+    #         print(f"Label: {labels[i]}")
+    #         print(f"State: {states[i]}")
+    #         print(f"--------------------------------")
+
+    bad_questions_res = []
+    print("Incorrect predictions:")
+    for i in range(len(states)):
+        if preds[i] != labels[i]:
+            bad_questions_res.append(bad_questions[i])
+    print(bad_questions_res)
+
+
+    print("Debug notes:")
+    for i in range(len(states)):
+        if preds[i] != labels[i]:
+            print(f"Question: {questions[i]}")
+            print(f"Pred: {preds[i]}")
+            print(f"Label: {labels[i]}")
+            print(f"State: {states[i]}")
+            print(f"--------------------------------")
 
     # Compute accuracy
     acc = np.mean(np.array(preds) == np.array(labels))
